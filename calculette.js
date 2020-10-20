eb3e10a6c07cea2c18a3e18b7da13fb62453e112
Greg <gregsino@gmail.com>
2020-10-20 20:32:01 +0200

    Initial commit

---
+++ calculette.js
@@ -0,0 +1,19 @@
+let addition = function(num1,num2) {
+    console.log("Addition : " + (num1+num2));
+}
+addition(2,2);
+
+let substraction = function(num1,num2) {
+    console.log("substraction : " + (num1-num2));
+}
+substraction(2,2);
+
+let division = function(num1,num2) {
+    console.log("division : " + (num1/num2));
+}
+division(2,2);
+
+let modulo = function(num1,num2) {
+    return("modulo of " + num1 + " and " + num2 + " = " + (num1 % num2));
+}
+console.log(modulo(5,2));
